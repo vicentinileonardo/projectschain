4a6003534f0a350f9ea64d785abce1154a634509
MatteoZanotto <matteozanotto286@gmail.com>
2023-06-04 15:18:44 +0200

    Add home

--- frontend/views/HomeView.vue
+++ frontend/views/HomeView.vue
@@ -1,5 +1,29 @@
 <script setup lang="ts">
+import { Icon } from '@iconify/vue';
+import InfoHeader from '@/components/InfoHeader.vue';
+import AppButton from '@/components/AppButton.vue';
+
 </script>
 
 <template>
+    <InfoHeader />
+
+    <div class="buttons-row">
+        <AppButton class="bg-primary">
+            <Icon icon="material-symbols:upload" />
+            Upload project
+        </AppButton>
+        <AppButton class="bg-primary">
+            <Icon icon="material-symbols:explore" />
+            Explore catalog
+        </AppButton>
+    </div>
 </template>
+
+<style scoped>
+.buttons-row {
+    display: flex;
+    gap: 0.25rem;
+    justify-content: center;
+}
+</style>
