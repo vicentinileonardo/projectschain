3a9b45c4793d8608d26ffd15703831360fff5ed7
lucavianunitn <72442491+lucavianunitn@users.noreply.github.com>
2023-06-08 11:19:58 +0200

    Update DesignerNFT.sol, added mintToken method

--- contracts/DesignerNFT.sol
+++ contracts/DesignerNFT.sol
@@ -4,25 +4,26 @@ pragma solidity ^0.8.19;
 import "./BaseNFT.sol";
 
 contract DesignerNFT is BaseNFT {
+    uint256 public tokenCounter;
     
-    constructor(string memory _projectId, address _owner, address[] memory _components) ERC721("DesignerNFT", "DNFT") {
-        setProjectId(_projectId);
-        setOwner(_owner);
-        setComponents(_components);
+    mapping(string => uint256) private tokenURIToTokenId;
+
+    constructor () ERC721 ("DesignerNFT", "DNFT"){
+        tokenCounter = 0;
     }
 
+    function mintToken(string memory uri) public returns (uint256) {
+        require(tokenURIToTokenId[uri] == 0); //the token URI is new, so the NFT about the same project wasn't already minted
 
+        uint256 newItemId = tokenCounter;
 
-    function mint(string memory tokenURI) public returns (uint256) {
-        string memory projectId = getProjectId();
-        bytes32 hash = keccak256(abi.encodePacked(projectId));
-        uint256 newTokenId = uint256(hash);
-        //_safeMint(to, newTokenId);
-        _mint(msg.sender, newTokenId);
-        _setTokenURI(newTokenId, tokenURI);
-        return newTokenId;
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, uri);
+
+        tokenCounter = tokenCounter + 1;
+        tokenURIToTokenId[uri] = newItemId;
+
+        return newItemId;
     }
-
-
 }
 
