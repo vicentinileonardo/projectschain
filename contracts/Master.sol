a0d341e17d3a601a6108f833adee982f64e35355
MatteoZanotto <matteozanotto286@gmail.com>
2023-06-21 15:03:40 +0200

    contracts: fix call signature on master

--- contracts/Master.sol
+++ contracts/Master.sol
@@ -1,24 +1,33 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "./ProjectNFT.sol";
+import './ProjectNFT.sol';
 
 contract Master {
-    address public projectNFTAddress;
+  address public projectNFTAddress;
 
-    constructor(address _contractAddress) {
-        projectNFTAddress = _contractAddress;
-    }
+  constructor(address _contractAddress) {
+    projectNFTAddress = _contractAddress;
+  }
 
-    //from the address of the contract A, we can call the function mintToken
-    function mintToken(string memory uri, uint256 price) public returns (bool){ //aggiungere hash
-        //TODO: check if the caller is authorized
-        
-        (bool success, bytes memory result) = projectNFTAddress.call(abi.encodeWithSignature("mintToken(address,string, uint256)", msg.sender, uri, price));
-        require(success, "Failed to call mintToken on DesignerNFT");
-        //address sender = abi.decode(result, (address));
+  //from the address of the contract A, we can call the function mintToken
+  function mintToken(
+    string calldata uri,
+    uint256 price,
+    uint256 royaltyPrice,
+    string calldata projectHash,
+    uint256[] calldata components
+  ) public returns (bool) {
+    //aggiungere hash
+    //TODO: check if the caller is authorized
 
-        return success;
-    }
+    (bool success, bytes memory result) = projectNFTAddress.call(
+      abi.encodeWithSignature('mintToken(address, string, uint256, uint256, string, uint256[])',
+       msg.sender, uri, price, royaltyPrice, projectHash, components)
+    );
+    require(success, 'Failed to call mintToken on DesignerNFT');
+    //address sender = abi.decode(result, (address));
 
+    return success;
+  }
 }
