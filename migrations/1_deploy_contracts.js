4e2bdd75958c23f4b384db97bdd2c5353d95cbe0
vicentinileonardo <vicentini.leonardo99@gmail.com>
2023-06-27 17:01:21 +0200

    update migrations

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,18 +1,16 @@
 require('dotenv').config();
 
-const pluginAddress = process.env.PLUGIN_ADDRESS; //"our" address where the commissions for the operations will be transferred
+const {PLUGIN_ADDRESS, HOST_MACHINE_IP, ORACLE, JOBID_1, JOBID_2} = process.env;
+
 const ProjectNFT = artifacts.require("ProjectNFT");
 const Master = artifacts.require("Master");
 const AccessSmartContract = artifacts.require("AccessSmartContract");
-const Counter = artifacts.require("Counter");
 
 module.exports = async function (deployer) {
-    await deployer.deploy(ProjectNFT,pluginAddress);
+    await deployer.deploy(ProjectNFT, PLUGIN_ADDRESS, HOST_MACHINE_IP, ORACLE, JOBID_1, JOBID_2);
     const projectNftDeployed = await ProjectNFT.deployed();
     await deployer.deploy(AccessSmartContract, projectNftDeployed.address);
     const accessContractDeployed = await AccessSmartContract.deployed();
     await deployer.deploy(Master, projectNftDeployed.address, accessContractDeployed.address);
 
-    // TODO remove counter address, was added for testing
-    await deployer.deploy(Counter);
 };
